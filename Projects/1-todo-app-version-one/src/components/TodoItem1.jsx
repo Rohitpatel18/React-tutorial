79eee349813735062a8eced8d6de5625f5542964
Rohitpatel18 <rohitpatel123p@gmail.com>
2024-03-14 18:11:05 +0530

    Component Todo App

---
+++ Projects/1-todo-app-version-one/src/components/TodoItem1.jsx
@@ -0,0 +1,25 @@
+function TodoItem1(){
+
+    let todoName='Buy Milk';
+    let tododate='4/10/2023';
+
+
+    return (
+    <div class="container ">
+      <div class="row kg-row ">
+        <div class="col-6 items">
+            {todoName}
+          </div>
+
+        <div class="col-4 items">
+          {tododate}
+        </div>
+        <div class="col-2">
+        <button type="button" class="btn btn-danger">delete</button>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+export default TodoItem1;
---
+++ Projects/1-todo-app-version-one/src/components/TodoItem2.jsx
@@ -0,0 +1,25 @@
+function TodoItem2(){
+
+    let todoName='Go to collage';
+    let tododate='4/10/2023';
+
+
+    return (
+    <div class="container ">
+      <div class="row kg-row ">
+        <div class="col-6 items">
+            {todoName}
+          </div>
+
+        <div class="col-4  items">
+          {tododate}
+        </div>
+        <div class="col-2">
+        <button type="button" class="btn btn-danger kg-button">delete</button>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+export default TodoItem2;
